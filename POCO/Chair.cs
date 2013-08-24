597d044a7e484d0a102a33bc758412e0c65c4766
meloera <pmcunnin@ucsd.edu>
2013-08-24 14:58:49 -0700

    Justin is bitch

--- POCO/Chair.cs
+++ POCO/Chair.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Runtime.Serialization; // this is required
 
-namespace POCO
+namespace POCO 
 {
     [DataContract]
     public class Chair// : Staff
@@ -38,6 +38,7 @@ namespace POCO
                    email + ";" +
                    password + ";" +
                    dept.id + ";";
+            //TESTING
         }
 
     }
