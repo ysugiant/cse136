80156db2e04509ab0c6a11dd3f3642cf32b4d6b9
ysugiant <ysugiant@ucsd.edu>
2013-08-24 16:54:19 -0700

    Enrollment(Not yet tested) and Department test

--- DAL/DALEnrollment.cs
+++ DAL/DALEnrollment.cs
@@ -13,7 +13,8 @@ namespace DAL
     {
         static string connection_string = ConfigurationManager.AppSettings["dsn"];
 
-        public static Transcript GetScheduleList(string id, ref List<string> errors)
+        //Done By student
+        public static Transcript GetEnrollment(string id, ref List<string> errors)
         {
             SqlConnection conn = new SqlConnection(connection_string);
             Transcript trans = new Transcript();
@@ -21,10 +22,8 @@ namespace DAL
             try
             {
                 //fill student info
-                trans.student = new Student();
-                //(Student)DAL.DALStudent.GetStudentDetail(id, errors);
-                //List<Student> t = DAL.DALStudent.GetStudentList(errors);
-                //DALStudent.GetStudentDetail(id, errors);
+                trans.student = DALStudent.GetStudentDetail(id, ref errors);
+
                 string strSQL = "spGetEnrollmentList";
 
                 SqlDataAdapter mySA = new SqlDataAdapter(strSQL, conn);
@@ -68,5 +67,101 @@ namespace DAL
 
             return trans;
         }
+
+        //Done by student
+        public static void InsertEnrollment(string student_id, int schedule_id, ref List<string> errors)
+        {
+            SqlConnection conn = new SqlConnection(connection_string);
+
+            string strSQL = "spInsertStudentSchedule";
+
+            try
+            {
+                SqlDataAdapter mySA = new SqlDataAdapter(strSQL, conn);
+                mySA.SelectCommand.CommandType = CommandType.StoredProcedure;
+                mySA.SelectCommand.Parameters.Add(new SqlParameter("@student_id", SqlDbType.VarChar, 20));
+                mySA.SelectCommand.Parameters.Add(new SqlParameter("@schedule_id", SqlDbType.Int));
+
+                mySA.SelectCommand.Parameters["@student_id"].Value = student_id;
+                mySA.SelectCommand.Parameters["@schedule_id"].Value = schedule_id;
+
+                DataSet myDS = new DataSet();
+                mySA.Fill(myDS);
+            }
+            catch (Exception e)
+            {
+                errors.Add("Error: " + e.ToString());
+            }
+            finally
+            {
+                conn.Dispose();
+                conn = null;
+            }
+        }
+
+        //Done by student
+        public static void DeleteEnrollment(string student_id, int schedule_id, ref List<string> errors)
+        {
+            SqlConnection conn = new SqlConnection(connection_string);
+
+            string strSQL = "spDeleteStudentSchedule";
+
+            try
+            {
+                SqlDataAdapter mySA = new SqlDataAdapter(strSQL, conn);
+                mySA.SelectCommand.CommandType = CommandType.StoredProcedure;
+                mySA.SelectCommand.Parameters.Add(new SqlParameter("@student_id", SqlDbType.VarChar, 20));
+                mySA.SelectCommand.Parameters.Add(new SqlParameter("@schedule_id", SqlDbType.Int));
+
+                mySA.SelectCommand.Parameters["@student_id"].Value = student_id;
+                mySA.SelectCommand.Parameters["@schedule_id"].Value = schedule_id;
+
+                DataSet myDS = new DataSet();
+                mySA.Fill(myDS);
+            }
+            catch (Exception e)
+            {
+                errors.Add("Error: " + e.ToString());
+            }
+            finally
+            {
+                conn.Dispose();
+                conn = null;
+            }
+
+        }
+
+        public static void UpdateEnrollment(string student_id, int schedule_id, string grade, ref List<string> errors)
+        {
+            SqlConnection conn = new SqlConnection(connection_string);
+
+            string strSQL = "spUpdateEnrollmentInfo";
+
+            try
+            {
+                SqlDataAdapter mySA = new SqlDataAdapter(strSQL, conn);
+                mySA.SelectCommand.CommandType = CommandType.StoredProcedure;
+                mySA.SelectCommand.Parameters.Add(new SqlParameter("@student_id", SqlDbType.VarChar, 20));
+                mySA.SelectCommand.Parameters.Add(new SqlParameter("@schedule_id", SqlDbType.Int));
+                mySA.SelectCommand.Parameters.Add(new SqlParameter("@grade", SqlDbType.VarChar, 10));
+
+                mySA.SelectCommand.Parameters["@student_id"].Value = student_id;
+                mySA.SelectCommand.Parameters["@schedule_id"].Value = schedule_id;
+                mySA.SelectCommand.Parameters["@grade"].Value = grade;
+
+                DataSet myDS = new DataSet();
+                mySA.Fill(myDS);
+            }
+            catch (Exception e)
+            {
+                errors.Add("Error: " + e.ToString());
+            }
+            finally
+            {
+                conn.Dispose();
+                conn = null;
+            }
+
+        } 
     }
 }
---
+++ DALTest/DALEnrollmentTest.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DAL;
+using POCO;
+
+namespace DALTest
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class DALEnrollmentTest
+    {
+        public DALEnrollmentTest()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        /// <summary>
+        ///A test for InsertStudent
+        ///</summary>
+        [TestMethod]
+        public void EnrollmentTest()
+        {
+            List<string> errors = new List<string>();
+
+            Student student = new Student();
+            student.id = Guid.NewGuid().ToString().Substring(0, 20); // use the existing student ID 
+            student.first_name = "first2";
+            student.last_name = " last2";
+            student.ssn = "777664321";
+            student.email = "myemail2@ucsd.edu";
+            student.password = "pass1234";
+            student.shoe_size = 2;
+            student.weight = 2;
+            student.major = 1;//JUSTIN ADDED THIS
+            student.level = StudentLevel.senior;//JUSTIN ADDED THIS
+            student.status = 0;//JUSTIN ADDED THIS
+
+            //System.Diagnostics.Debug.WriteLine("value of student.level is: " + student.level.ToString());
+            DALStudent.InsertStudent(student, ref errors);
+
+            List<ScheduleCourse> scheduleList = DALCourseSchedule.GetScheduleList("", "", ref errors);
+            Assert.AreEqual(0, errors.Count);
+
+            // enroll all available scheduled courses for this student
+            for (int i = 0; i < scheduleList.Count; i++)
+            {
+                // this enrolls the student into one course at a time, hence the for loop
+                DALEnrollment.InsertEnrollment(student.id, scheduleList[i].id, ref errors);
+                Assert.AreEqual(0, errors.Count);
+            }
+
+            Transcript trans = DALEnrollment.GetEnrollment(student.id, ref errors);
+
+            for (int i = 0; i < trans.grade.Count; i++)
+            {
+                Assert.AreEqual(trans.grade[i].course, scheduleList[i].id);
+            }
+
+            // drop all available scheduled courses for this student
+            for (int i = 0; i < scheduleList.Count; i++)
+            {
+                DALEnrollment.DeleteEnrollment(student.id, scheduleList[i].id, ref errors);
+                Assert.AreEqual(0, errors.Count);
+            }
+
+            DALStudent.DeleteStudent(student.id, ref errors);
+        }
+
+    }
+}
