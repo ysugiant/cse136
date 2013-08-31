12b137be0aa1a927024414674d5bd498039f13d6
meloera <pmcunnin@ucsd.edu>
2013-08-30 20:34:03 -0700

    Add BL Tests for Major and Course Schedule, still need to implement them.

---
+++ BLTest/BLMajorTest.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using POCO;
+using BL;
+
+namespace BLTest
+{
+  /// <summary>
+  /// Summary description for UnitTest1
+  /// </summary>
+  [TestClass]
+  public class BLMajorTest
+  {
+    public BLMajorTest()
+    {
+      //
+      // TODO: Add constructor logic here
+      //
+    }
+
+    private TestContext testContextInstance;
+
+    /// <summary>
+    ///Gets or sets the test context which provides
+    ///information about and functionality for the current test run.
+    ///</summary>
+    public TestContext TestContext
+    {
+      get
+      {
+        return testContextInstance;
+      }
+      set
+      {
+        testContextInstance = value;
+      }
+    }
+
+    #region Additional test attributes
+    //
+    // You can use the following additional attributes as you write your tests:
+    //
+    // Use ClassInitialize to run code before running the first test in the class
+    // [ClassInitialize()]
+    // public static void MyClassInitialize(TestContext testContext) { }
+    //
+    // Use ClassCleanup to run code after all tests in a class have run
+    // [ClassCleanup()]
+    // public static void MyClassCleanup() { }
+    //
+    // Use TestInitialize to run code before running each test 
+    // [TestInitialize()]
+    // public void MyTestInitialize() { }
+    //
+    // Use TestCleanup to run code after each test has run
+    // [TestCleanup()]
+    // public void MyTestCleanup() { }
+    //
+    #endregion
+
+    [TestMethod]
+    public void BusinessLayerMajorTest()
+    {/*
+      List<string> errors = new List<string>();
+
+      BLStudent.InsertStudent(null, ref errors);
+      Assert.AreEqual(1, errors.Count);
+
+      errors = new List<string>();
+
+      Student student = new Student();
+      student.id = "";
+      BLStudent.InsertStudent(student, ref errors);
+      Assert.AreEqual(1, errors.Count); */
+    }
+
+    [TestMethod]
+    public void BusinessLayerMajorErrorTest()
+    {
+
+    }
+  }
+}
