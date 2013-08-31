32579ec0cdd48c29b89a5bdb72fba4d10a7a2079
ysugiant <ysugiant@ucsd.edu>
2013-08-30 17:07:34 -0700

    Service Layer for Enrollment and Department

---
+++ SL136/ISLDepartment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using POCO; // this is required
+using BL; // this is required
+
+namespace SL136
+{
+  // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+  [ServiceContract]
+  public interface ISLDepartment
+  {
+
+    [OperationContract]
+    Department GetDepartmentDetail(string name, ref List<string> errors);
+
+    [OperationContract]
+    void InsertDepartment(Department dept, ref List<string> errors);
+
+    [OperationContract]
+    void DeleteDepartment(string name, ref List<string> errors);
+
+    [OperationContract]
+    void UpdateDepartment(Department dept, ref List<string> errors);
+
+  }
+}
---
+++ SL136/SLEnrollment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using POCO;
+using BL;
+
+namespace SL136
+{
+  // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "SLStudent" in both code and config file together.
+  public class SLEnrollment : ISLEnrollment
+  {
+    public void InsertEnrollment(string student_id, int schedule_id, ref List<string> errors)
+    {
+      BLEnrollment.InsertEnrollment(student_id, schedule_id, ref errors);
+    }
+
+    public void DeleteEnrollment(string student_id, int schedule_id, ref List<string> errors)
+    {
+      BLEnrollment.DeleteEnrollment(student_id, schedule_id, ref errors);
+    }
+
+    public List<Enrollment> GetEnrollmentList(string student_id, ref List<string> errors)
+    {
+      return BLEnrollment.GetEnrollment(student_id, ref errors);
+    }
+
+    public void UpdateEnrollment(string student_id, int schedule_id, string grade, ref List<string> errors)
+    {
+        BLEnrollment.UpdateEnrollment(student_id, schedule_id, grade, ref errors);
+    }
+  }
+}
