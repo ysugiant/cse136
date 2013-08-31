b49706e540334d2484fc41a29892515f13cab750
meloera <pmcunnin@ucsd.edu>
2013-08-30 17:29:45 -0700

    Completed BLMajor

--- BL/BLMajor.cs
+++ BL/BLMajor.cs
@@ -10,57 +10,43 @@ namespace BL
     public static class BLMajor
     {
         public static void InsertMajor(string majorName, int deptID, ref List<string> errors)
-        {/*
-            BLCheck.checkStaffErrors(staffMember, ref errors);
-
-            int newStaffID;
-            DALStaff.InsertStaff(staffMember, ref errors, out newStaffID);*/
-        }
-
-        public static void UpdateMajor(int majorID, string majorName, int deptID, ref List<string> errors)
-        {/*
-            if (staffMember == null)
-            {
-                errors.Add("Staff member cannot be null");
-            }
-
-            if (staffMember.id < 5)
-            {
-                errors.Add("Invalid staff ID");
-            }
-
+        {
+            BLCheck.checkMajorName(majorName, ref errors);
+            BLCheck.checkDeptID(deptID, ref errors);
             if (errors.Count > 0)
                 return;
 
-            DALStaff.UpdateStaff(staffMember, ref errors); */
+            int majorID;
+            DALMajor.InsertMajor(majorName, deptID, ref errors, out majorID);   
+        }
+
+        public static void UpdateMajor(int majorID, string majorName, int deptID, ref List<string> errors)
+        {
+            BLCheck.checkMajorID(majorID, ref errors);
+            BLCheck.checkMajorName(majorName, ref errors);
+            BLCheck.checkDeptID(deptID, ref errors);
+            if (errors.Count > 0)
+                return;
+
+            DALMajor.UpdateMajor(majorID, majorName, deptID, ref errors);
         }
 
         public static Major GetMajorDetail(int id, ref List<string> errors)
-        {/*
-            if (id < 0)
-            {
-                errors.Add("Invalid staff ID. ID must be a non-negative integer.");
-            }
-
-            // anything else to validate?
-
+        {
+            BLCheck.checkMajorID(id, ref errors);
             if (errors.Count > 0)
                 return null;
-            */
+            
             return (DALMajor.GetMajorDetail(id, ref errors));
         }
 
         public static void DeleteMajor(int id, ref List<string> errors)
-        {/*
-            if (id < 0)
-            {
-                errors.Add("Invalid staff ID. ID must be a non-negative integer.");
-            }
-
-            if (errors.Count > 0s)
+        {
+            BLCheck.checkMajorID(id, ref errors);
+            if (errors.Count > 0)
                 return;
-
-            DALStaff.DeleteStaff(id, ref errors);*/
+            
+            DALMajor.DeleteMajor(id, ref errors);
         }
 
         public static List<Tuple<string, string>> GetMajorList(ref List<string> errors)
