06c7162afb78bbb44b04e6da4bea338779970be9
ysugiant <ysugiant@ucsd.edu>
2013-09-03 21:24:20 -0700

    Department POCO modification

--- DAL/DALDepartment.cs
+++ DAL/DALDepartment.cs
@@ -102,17 +102,6 @@ namespace DAL
                 dept.deptName = myDS.Tables[0].Rows[0]["dept_name"].ToString();
                 dept.chairID = Convert.ToInt32(myDS.Tables[0].Rows[0]["chair_id"].ToString());
 
-                if (myDS.Tables[1] != null)
-                {
-                    dept.majorList = new List<String>();
-                    for (int i = 0; i < myDS.Tables[1].Rows.Count; i++)
-                    {
-                        string maj = "";
-                        maj = myDS.Tables[1].Rows[i]["major_name"].ToString();
-                        
-                        dept.majorList.Add(maj);
-                    }
-                }
             }
             catch (Exception e)
             {
@@ -125,6 +114,46 @@ namespace DAL
             }
 
             return dept;
+        }
+
+        public static List<Department> GetDepartmentList(ref List<string> errors)
+        {
+            SqlConnection conn = new SqlConnection(connection_string);
+            List<Department> deptList = new List<Department>();
+
+            try
+            {
+                string strSQL = "spGetDepartmentInfo";
+
+                SqlDataAdapter mySA = new SqlDataAdapter(strSQL, conn);
+                mySA.SelectCommand.CommandType = CommandType.StoredProcedure;
+
+                DataSet myDS = new DataSet();
+                mySA.Fill(myDS);
+
+                if (myDS.Tables[0].Rows.Count == 0)
+                    return null;
+
+                for (int i = 0; i < myDS.Tables[0].Rows.Count; i++)
+                {
+                    Department dept = new Department();
+                    dept.id = Convert.ToInt32(myDS.Tables[0].Rows[0]["dept_id"].ToString());
+                    dept.deptName = myDS.Tables[0].Rows[0]["dept_name"].ToString();
+                    dept.chairID = Convert.ToInt32(myDS.Tables[0].Rows[0]["chair_id"].ToString());
+                    deptList.Add(dept);
+                }
+            }
+            catch (Exception e)
+            {
+                errors.Add("Error: " + e.ToString());
+            }
+            finally
+            {
+                conn.Dispose();
+                conn = null;
+            }
+
+            return deptList;
         }
 
         public static void DeleteDepartment(string name, ref List<string> errors)
