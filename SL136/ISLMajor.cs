1df70585e8dac52771463a738a555b6b93345151
carter213 <passcut213@gmail.com>
2013-09-03 23:48:34 -0700

    create SL MAJOR and COurse SChedule

---
+++ SL136/ISLMajor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using POCO;
+using BL;
+
+namespace SL136
+{
+    class ISLMajor
+    {
+        [OperationContract]
+         List<Tuple<string, string>> GetMajorList(ref List<string> errors);
+
+        [OperationContract]
+        void InsertMajor(Major major, ref List<string> errors, out int ID);
+
+        [OperationContract]
+       void UpdateMajor(Major major, ref List<string> errors);
+
+        [OperationContract]
+        void DeleteMajor(int id, ref List<string> errors);
+
+        [OperationContract]
+        Major GetMajorDetail(int id, ref List<string> errors);
+
+    }
+}
---
+++ SL136/SLMajor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using POCO;
+using BL;
+
+namespace SL136
+{
+    class SLMajor
+    {
+        public List<Tuple<string, string>> GetMajorList(ref List<string> errors)
+        {
+            return BLMajor.GetMajorList(ref errors);
+        }
+
+        public void InsertMajor(Major major, ref List<string> errors, out int ID)
+        {
+             BLMajor.InsertMajor(major , ref errors, out ID);
+        }
+
+        public void UpdateMajor(Major major, ref List<string> errors)
+        {
+            BLMajor.UpdateMajor(major, ref errors);
+        }
+
+        public void DeleteMajor(int id, ref List<string> errors)
+        {
+            BLMajor.DeleteMajor(id, ref errors);
+        }
+
+        public Major GetMajorDetail(int id, ref List<string> errors)
+        {
+           return BLMajor.GetMajorDetail(id, ref errors);
+        }
+    }
+}
